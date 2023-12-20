19584b1dec7bb02b8a72bb9f8ff8098b1080d5db
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-12-20 21:41:24 +0200

    models

--- models/Contact.js
+++ models/Contact.js
@@ -1,6 +1,8 @@
 import { Schema, model } from "mongoose";
 import Joi from 'joi';
 
+import {handleSaveError, addUpdateSettings} from "./hooks.js";
+
 const contactSchema = new Schema({
     name: {
         type: String,
@@ -16,7 +18,13 @@ const contactSchema = new Schema({
         type: Boolean,
         default: false,
     },
-}, {versionKey: false, timestamps: true});
+}, { versionKey: false, timestamps: true });
+
+contactSchema.post("save", handleSaveError);
+
+contactSchema.pre("findOneAndUpdate", addUpdateSettings);
+
+contactSchema.post("findOneAndUpdate", handleSaveError);
 
 export const contactPostSchema = Joi.object({
   name: Joi.string().required().messages({ "any.required": "missing required name field"}),
