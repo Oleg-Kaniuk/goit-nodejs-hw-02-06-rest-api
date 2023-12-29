618e8b91ff999744883c51e9880e8916f413c9b4
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-12-29 22:18:18 +0200

    subscriptionSchema

--- models/User.js
+++ models/User.js
@@ -42,6 +42,10 @@ export const userSigninSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required().messages({ "any.required": "missing required email field"}),
 });
 
+export const userSubscriptionSchema = Joi.object({
+    subscription: Joi.string().valid("starter", "pro", "business").required()
+});
+
 const User = model("user", userSchema);
 
 export default User;
