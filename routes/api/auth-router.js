0e8f10a7cb8fdbde382abf20c5b5ea5153a8b82c
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-12-29 22:40:00 +0200

    router

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,11 +2,11 @@ import express from "express";
 
 import authController from "../../controllers/auth-controller.js";
 
-import {isEmptyBody, isValidId, authenticate} from "../../middlewares/index.js";
+import {isEmptyBody, authenticate} from "../../middlewares/index.js";
 
 import {validateBody} from "../../decorators/index.js";
 
-import { userSignupSchema, userSigninSchema } from "../../models/User.js";
+import { userSignupSchema, userSigninSchema, userSubscriptionSchema } from "../../models/User.js";
 
 const authRouter = express.Router();
 
@@ -17,5 +17,7 @@ authRouter.post("/login", isEmptyBody, validateBody(userSigninSchema), authContr
 authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/logout", authenticate, authController.logout);
+
+authRouter.patch("/", authenticate, isEmptyBody, validateBody(userSubscriptionSchema), authController.subscription);
 
 export default authRouter;
