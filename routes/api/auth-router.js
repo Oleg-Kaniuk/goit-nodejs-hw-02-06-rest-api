f3788cb0b4f32ce8d3b46f61e69a1c4f54a8276c
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2024-01-16 15:31:57 +0200

    routes

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -6,11 +6,15 @@ import {isEmptyBody, authenticate, upload} from "../../middlewares/index.js";
 
 import {validateBody} from "../../decorators/index.js";
 
-import { userSignupSchema, userSigninSchema, userSubscriptionSchema } from "../../models/User.js";
+import { userSignupSchema, userSigninSchema, userSubscriptionSchema, userEmailSchema } from "../../models/User.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", isEmptyBody, validateBody(userSignupSchema), authController.signup);
+
+authRouter.get("/verify/:verificationToken", authController.verify);
+
+authRouter.post("/verify", isEmptyBody, validateBody(userEmailSchema), authController.resendVerifyEmail);
 
 authRouter.post("/login", isEmptyBody, validateBody(userSigninSchema), authController.signin);
 
